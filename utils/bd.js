a9f3c6442b273bef40306d668aab6eb087bb67de
Paula Alvarez <paula.alvarez@avature.net>
2022-10-27 20:21:45 -0300

    General Structure - Connection to Mongo

---
+++ utils/bd.js
@@ -0,0 +1,13 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const pool = async() => {
+    try {
+        return(await MongoClient.connect("mongodb://localhost:27017")).db(
+            "partidos"
+        );
+    } catch(e){
+        console.error(e.stack)
+    }
+}
+
+module.exports = { pool };
